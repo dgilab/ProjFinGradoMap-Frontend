d6fd4024b211d5cad9ddc96a6b805dce6b57407a
Diego Gil López <dgil@laberit.com>
2024-04-26 12:52:01 +0200

    Simplificación de estilos y reorganización de las clases

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,22 @@
 <style lang="scss">
 :root{
 
+	--build-color-black: #000000;
+	--build-color-white: #ffffff;
+
+
+
+	--build-color-basePabellones: #ffffff;
+	--build-color-baseMapa: #d6d5d1;
+	--build-color-marcadores-grandes: #000000;
+	--build-color-marcadores-pequenos: #e7e7e7;
+	--build-color-escenarios: #f0ec2d;
+	--build-color-cafeteias: #fdbe18;
+	--build-color-servicios: #6e91f8;
+	--build-color-stands: #257c68;
+	--build-color-salidasEmergencia: #23a55b;
+	--build-color-foodTruck: #f60;
+	
 }
 body {
 	margin: 0;
@@ -14,117 +30,18 @@ body {
 	font-family: 'Avenir Next', Avenir, 'Helvetica Neue', Helvetica, Arial, sans-serif;
 }
 
-.cls_1 {
-	fill: #000;
-}
-
-.cls_1, .cls_2, .cls_3, .cls_4, .cls_5, .cls_6, .cls_7 {
-	stroke-width: 0px;
-}
-
-.cls_8 {
-	stroke-width: .3px;
-}
-
-.cls_8, .cls_9, .cls_10, .cls_11, .cls_12, .cls_13, .cls_14, .cls_15, .cls_16, .cls_17 {
-	stroke: #000;
-}
-
-.cls_8, .cls_10, .cls_11, .cls_13, .cls_18, .cls_6 {
-	fill: #fff;
-}
-
-.cls_9 {
-	stroke-width: .2px;
-}
-
-.cls_9, .cls_12, .cls_15, .cls_16 {
-	fill: #fdbe18;
-}
-
-.cls_10 {
-	stroke-width: .3px;
-}
-
-.cls_19 {
-	opacity: 1;
-}
-
-.cls_2 {
-	fill: #257c68;
-}
-
-.cls_3 {
-	fill: #23a55b;
-}
-
-.cls_4 {
-	fill: #f0ec2d;
-}
-
-.cls_5 {
-	fill: #313952;
-}
-
-.cls_11 {
-	stroke-width: .3px;
-}
-
-.cls_20 {
-	fill: #dff2fd;
-	stroke: #1d1d1b;
-}
-
-.cls_20, .cls_21 {
-	stroke-miterlimit: 10;
-}
-
-.cls_12 {
-	stroke-width: .1px;
-}
-
-.cls_13 {
-	stroke-width: .3px;
-}
-
-.cls_14 {
-	fill: #e7e7e7;
-	stroke-width: .3px;
-}
-
-.cls_18 {
-	font-family: MyriadPro-Regular, 'Myriad Pro';
-	font-size: 21px;
-}
-
-.cls_15 {
-	stroke-width: .1px;
-}
-
-.cls_16 {
-	stroke-width: 0px;
-}
-
-.cls_21 {
-	fill: none;
-	stroke: #fff;
-	stroke-width: 2px;
-}
-
-.cls_7 {
-	fill: #f60;
-}
-
-.cls_17 {
-	fill: #d6d5d1;
-	stroke-width: .3px;
-}
-
-
-
-
-
-
+.cls_1{fill: var(--build-color-marcadores-grandes);}
+.cls_2{fill: var(--build-color-stands);}
+.cls_3{fill: var(--build-color-salidasEmergencia);}
+.cls_4{fill: var(--build-color-escenarios);}
+.cls_5{fill: var(--build-color-servicios);}
+.cls_7{fill: var(--build-color-foodTruck);}
+.cls_8{fill: var(--build-color-basePabellones);stroke-width: .3px;stroke: var(--build-color-black);} //Pabellones Base
+.cls_9{fill: var(--build-color-cafeteias);stroke-width: .2px;stroke: var(--build-color-black);}
+.cls_13{fill: var(--build-color-white);stroke-width: .3px;stroke: var(--build-color-black);} // Color Texto
+.cls_14{fill: var(--build-color-marcadores-pequenos);stroke-width: .3px;stroke: var(--build-color-black);}
+.cls_17{fill: var(--build-color-baseMapa);stroke-width: .3px;stroke: var(--build-color-black);}
+.cls_19{opacity: 1;}
 
 
 </style>
--- src/components/ExpoMapComp/Layers/BaseLayers/Pabellones/N2Pabellon3/Pabellon3Layer.vue
+++ src/components/ExpoMapComp/Layers/BaseLayers/Pabellones/N2Pabellon3/Pabellon3Layer.vue
@@ -7,8 +7,8 @@
 		<g id="Pabellon3">
 			<path id="activp3_01" data-name="activp3 01" class="cls_9" d="M288.25,57.25h153.8c2,0,3.8.88,5.1,2.29s2.1,3.36,2.1,5.51v311.4c0,2.15-.8,4.1-2.1,5.51s-3.1,2.29-5.1,2.29h-153.8c-2,0-3.8-.88-5.1-2.29s-2.1-3.36-2.1-5.51V65.05c0-2.15.8-4.1,2.1-5.51s3.1-2.29,5.1-2.29h0Z"/>
 			<g id="tp3_01" data-name="tp3 01" class="cls_13">
-				<text class="cls_13" transform="translate(301.35 214.05)"><tspan class="cls_11" x="0" y="0">A</tspan><tspan x="12.45" y="0">TRACCIONES</tspan></text>
-				<text class="cls_13" transform="translate(324.95 239.25)"><tspan x="0" y="0">DE FERI</tspan><tspan class="cls_15" x="82.84" y="0">A</tspan></text>
+				<text class="cls_13" transform="translate(301.35 214.05)"><tspan x="0" y="0">ATRACCIONES</tspan></text>
+				<text class="cls_13" transform="translate(324.95 239.25)"><tspan x="0" y="0">DE FERIA</tspan></text>
 			</g>
 		</g>
 	</g>
